993111753a27e114446373638cdad40df3ad89a7
vishwasransing <vishwasransing@gmail.com>
2023-11-28 17:43:58 +0530

    Content Negotiation
    -Added Internationalization, accept-languages = {en, hn, fr, nl}

--- src/main/java/com/learn/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/learn/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,7 +3,10 @@ package com.learn.rest.webservices.restfulwebservices.user;
 import java.net.URI;
 import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
+import java.util.Locale;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,44 +20,54 @@ import jakarta.validation.Valid;
 
 @RestController
 public class UserResource {
-	
-	UserDaoService service;  
-	
-	public UserResource(UserDaoService service) {
+
+	UserDaoService service;
+
+	MessageSource messageSource;
+
+	public UserResource(UserDaoService service, MessageSource messageSource) {
 		this.service = service;
+		this.messageSource = messageSource;
 	}
-	
+
 	@GetMapping("/users")
 	public List<User> retrieveAllUsers() {
 		return service.findAll();
 	}
-	
+
 	@GetMapping("/users/{id}")
 	public User retriveUser(@PathVariable int id) {
 		User user = service.findOneUser(id);
-		
-		if(user == null)
+
+		if (user == null)
 			throw new UserNotFoundException("User not found! ID :: " + id);
-			
+
 		return user;
 	}
-	
+
 	@PostMapping("/users")
 	public ResponseEntity<User> createNewUser(@Valid @RequestBody User user) {
-		
+
 		User savedUser = service.save(user);
-		
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-						.path("/{id}")
-						.buildAndExpand(savedUser.getId())
-						.toUri();
-		
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+
 		return ResponseEntity.created(location).build();
 	}
-	
+
 	@DeleteMapping("/users/{id}")
 	public void deleteUser(@PathVariable int id) {
 		service.deleteById(id);
 	}
-	
+
+	@GetMapping("/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+
+		Locale locale = LocaleContextHolder.getLocale();
+
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
+	}
+
 }
